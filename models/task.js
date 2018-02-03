36b408f6b369c323627c35bdc2b314fbde46e2ad
v123582 <v123582@gmail.com>
2018-02-03 11:17:14 +0800

    update

--- models/task.js
+++ models/task.js
@@ -1,7 +1,8 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var Task = sequelize.define('Task', {
-    title: DataTypes.STRING
+    title: DataTypes.STRING,
+    user: DataTypes.STRING,
   }, {
     classMethods: {
       associate: function(models) {
---
+++ seeders/20180203031257-my-seed-file.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('Tasks', [{
+        title: '測試 C',
+        createdAt : new Date(),
+        updatedAt : new Date(),
+        user: 'ABC',
+      }], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+  }
+};
