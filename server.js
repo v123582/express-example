9f1c35779fca7c8656281abb43d93edf22c0b785
v123582 <v123582@gmail.com>
2018-02-03 11:29:31 +0800

    update

--- server.js
+++ server.js
@@ -3,6 +3,57 @@ var app = express();
 
 var db = require('./models');
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
 app.listen(3000, function() {
   db.sequelize.sync();
+});
+
+var Task = db.Task;
+
+app.get('/tasks', function(req, res) {
+  Task.findAll()
+    .then(function (tasks) {
+      res.json(tasks);
+    });
+});
+
+app.get('/task/:id', function(req, res) {
+  Task.findById(req.params.id)
+    .then(function (task) {
+      res.json(task);
+    });
+});
+
+app.post('/task', function(req, res) {
+  Task.create({
+      title: req.title, 
+      createdAt : new Date(),
+      updatedAt : new Date(),
+    })
+    .then(function (task) {
+      res.json(task);
+    });
+});
+
+app.put('/task/:id', function(req, res) {
+  Task.findById(req.params.id)
+    .then(function (task) {
+      task.updateAttributes(req.body)
+        .then(function(task) {
+          res.json(task);
+        });
+    });
+});
+
+app.delete('/task/:id', function(req, res) {
+  Task.findById(req.params.id)
+    .then(function (task) {
+      task.destroy()
+        .then(function(task) {
+          res.json(task);
+        });
+    });
 });
